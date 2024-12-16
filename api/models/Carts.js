f5a798bddafa4e2bd2d3bfdb210639e837b99f22
Kien <kientrancc8@gmail.com>
2024-12-16 11:33:24 +0700

    feat: remove cart field

--- api/models/Carts.js
+++ api/models/Carts.js
@@ -1,39 +1,27 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+  const mongoose = require("mongoose");
+  const { Schema } = mongoose;
 
-const cartSchema = new Schema(
-  {
-    productItemId: {
-      type: Schema.Types.ObjectId, // Referencing the Menu schema
-      ref: "Product",
-      required: true,
+  const cartSchema = new Schema(
+    {
+      productItemId: {
+        type: Schema.Types.ObjectId, // Referencing the Menu schema
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+      email: {
+        type: String,
+        required: true,
+        trim: true,
+      },
     },
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-      minlength: 3,
-    },
-    recipe: String,
-    image: String,
-    price: {
-      type: Number,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-      min: 1,
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+  );
 
-const Cart = mongoose.model("Cart", cartSchema);
+  const Cart = mongoose.model("Cart", cartSchema);
 
-module.exports = Cart;
+  module.exports = Cart;
