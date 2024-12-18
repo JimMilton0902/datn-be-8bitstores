733a2faa18e53c5a173e0950741259f99b3558af
Kien <kientrancc8@gmail.com>
2024-12-18 16:18:03 +0700

    feat: update blog controller

--- api/routes/blogRoutes.js
+++ api/routes/blogRoutes.js
@@ -1,11 +1,12 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const blogController = require("../controller/blogController")
+const blogController = require("../controller/blogController");
 
-router.get("/", blogController.getAllBlog)
-router.post("/", blogController.postBlogs)
-router.get("/:id", blogController.singleBlogs)
-router.delete("/:id", blogController.deletedBlogs)
+router.get("/", blogController.getAllBlog);
+router.post("/", blogController.postBlogs);
+router.get("/:id", blogController.singleBlogs);
+router.delete("/:id", blogController.deletedBlogs);
+router.patch("/:id", blogController.updateBlog);
 
-module.exports = router
+module.exports = router;
